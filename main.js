73c8c0c36ce7394b2a5edb10e450c964c0f1f330
Johnny Briones <jbrrii91@gmail.com>
2020-09-23 00:11:21 -0400

    eventListeners

--- main.js
+++ main.js
@@ -1,1 +1,54 @@
-console.log('checking JavaScript Link');
+document.getElementById('one').addEventListener('click', function (e) {
+    console.log('1');
+})
+document.getElementById('two').addEventListener('click', function (e) {
+    console.log('2');
+})
+document.getElementById('three').addEventListener('click', function (e) {
+    console.log('3');
+})
+document.getElementById('four').addEventListener('click', function (e) {
+    console.log('4');
+})
+document.getElementById('five').addEventListener('click', function (e) {
+    console.log('5');
+})
+document.getElementById('six').addEventListener('click', function (e) {
+    console.log('6');
+})
+document.getElementById('seven').addEventListener('click', function (e) {
+    console.log('7');
+})
+document.getElementById('eight').addEventListener('click', function (e) {
+    console.log('8');
+})
+document.getElementById('nine').addEventListener('click', function (e) {
+    console.log('9');
+})
+document.getElementById('zero').addEventListener('click', function (e) {
+    console.log('0');
+})
+document.getElementById('plus').addEventListener('click', function (e) {
+    console.log('+');
+})
+document.getElementById('subtract').addEventListener('click', function (e) {
+    console.log('-');
+})
+document.getElementById('multiply').addEventListener('click', function (e) {
+    console.log('x');
+})
+document.getElementById('point').addEventListener('click', function (e) {
+    console.log('.');
+})
+document.getElementById('equal').addEventListener('click', function (e) {
+    console.log('=');
+})
+document.getElementById('divide').addEventListener('click', function (e) {
+    console.log('/');
+})
+document.getElementById('del').addEventListener('click', function (e) {
+    console.log('delete');
+})
+document.getElementById('all-clear').addEventListener('click', function (e) {
+    console.log('AC');
+})
